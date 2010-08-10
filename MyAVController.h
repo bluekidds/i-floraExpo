11a152696b7789e91595971ca2816d77d8bb97af
chun <chun.evavision@gmail.com>
2010-08-10 22:00:42 +0800

    test

---
+++ MyAVController.h
@@ -0,0 +1,50 @@
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <CoreVideo/CoreVideo.h>
+#import <CoreMedia/CoreMedia.h>
+
+/*!
+ @class	AVController 
+ @author Benjamin Loulier
+ 
+ @brief    Controller to demonstrate how we can have a direct access to the camera using the iPhone SDK 4
+ */
+@interface MyAVController : UIViewController <AVCaptureVideoDataOutputSampleBufferDelegate> {
+	AVCaptureSession *_captureSession;
+	UIImageView *_imageView;
+	CALayer *_customLayer;
+	AVCaptureVideoPreviewLayer *_prevLayer;
+	UILabel *_timeLabel;
+	UILabel *countLabel;
+	CFAbsoluteTime _startTime;
+	CFAbsoluteTime _currentTime;
+	int _count;
+}
+
+/*!
+ @brief	The capture session takes the input from the camera and capture it
+ */
+@property (nonatomic, retain) AVCaptureSession *captureSession;
+
+/*!
+ @brief	The UIImageView we use to display the image generated from the imageBuffer
+ */
+@property (nonatomic, retain) UIImageView *imageView;
+/*!
+ @brief	The CALayer we use to display the CGImageRef generated from the imageBuffer
+ */
+@property (nonatomic, retain) CALayer *customLayer;
+/*!
+ @brief	The CALAyer customized by apple to display the video corresponding to a capture session
+ */
+@property (nonatomic, retain) AVCaptureVideoPreviewLayer *prevLayer;
+@property (nonatomic, retain) UILabel *_timeLabel;
+//@property (nonatomic, assign) CFAbsoluteTime startTime;
+//@property (nonatomic, assign) CFAbsoluteTime currentTime;
+/*!
+ @brief	This method initializes the capture session
+ */
+- (void)initCapture;
+
+@end
